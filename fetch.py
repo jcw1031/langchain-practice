4530b78c62f6d66ca064ac5a624146aa8648b4d8
jcw1031 <jcw001031@gmail.com>
2025-05-18 23:37:11 +0900

    파일 제거

--- fetch.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get("https://www.kongju.ac.kr/bbs/KNU/2132/409495/artclView.do").text
-soup = BeautifulSoup(response, "html.parser")
-notices = [tag.text.strip() for tag in soup.select(".view-con")]
-print(notices)
